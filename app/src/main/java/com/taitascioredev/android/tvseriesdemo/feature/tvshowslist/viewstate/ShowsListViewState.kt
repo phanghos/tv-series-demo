c632ad1e3efc965a180408d1368e4462125d31f0
Roberto Tatasciore <rrtatasciore@opentrends.net>
2018-01-31 08:41:29 +0100

    Add view state class

---
+++ app/src/main/java/com/taitascioredev/android/tvseriesdemo/feature/tvshowslist/viewstate/ShowsListViewState.kt
@@ -0,0 +1,24 @@
+package com.taitascioredev.android.tvseriesdemo.feature.tvshowslist.viewstate
+
+import com.taitascioredev.android.tvseriesdemo.domain.model.MovieDbTvShow
+import com.taitascioredev.android.tvseriesdemo.presentation.mvibase.MviViewState
+
+/**
+ * Created by rrtatasciore on 24/01/18.
+ */
+data class ShowsListViewState(
+        val loading: Boolean,
+        val shows: List<MovieDbTvShow>?,
+        val error: Throwable?
+) : MviViewState {
+
+    companion object {
+        fun create(loading: Boolean, shows: List<MovieDbTvShow>?, error: Throwable?): ShowsListViewState {
+            return ShowsListViewState(loading, shows, error)
+        }
+        fun idle() = create(false, null, null)
+        fun success(shows: List<MovieDbTvShow>) = create(false, shows, null)
+        fun error(error: Throwable) = create(false, null, error)
+        fun inFlight() = create(true, null, null)
+    }
+}
