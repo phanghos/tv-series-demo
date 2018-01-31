0bfd9041ee5caba03e078833a813c89a9e4cd6fd
Roberto Tatasciore <rrtatasciore@opentrends.net>
2018-01-31 09:25:21 +0100

    Moved mergeResults out of the repository  to the action processor class

--- app/src/main/java/com/taitascioredev/android/tvseriesdemo/data/repository/impl/MovieRepositoryImpl.kt
+++ app/src/main/java/com/taitascioredev/android/tvseriesdemo/data/repository/impl/MovieRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.taitascioredev.android.tvseriesdemo.data.repository.impl
 
+import com.taitascioredev.android.tvseriesdemo.R.id.list
 import com.taitascioredev.android.tvseriesdemo.data.entity.mapper.ShowsListMapper
 import com.taitascioredev.android.tvseriesdemo.data.net.MovieDbService
 import com.taitascioredev.android.tvseriesdemo.data.repository.MovieRepository
@@ -16,15 +17,9 @@ class MovieRepositoryImpl @Inject constructor(
         private val mapper: ShowsListMapper
 ) : MovieRepository {
 
-    private val list: List<MovieDbTvShow> = ArrayList()
-
     override fun getPopularTvShows(page: Int): Observable<List<MovieDbTvShow>> {
         return service.getPopularTvShows(page)
-                .map {
-                    val newList = mapper.map(it)
-                    mergeResults(newList)
-                    list
-                }
+                .map { mapper.map(it) }
     }
 
     override fun getSimilarTvShows(showId: Int, page: Int): Observable<List<MovieDbTvShow>> {
@@ -33,9 +28,5 @@ class MovieRepositoryImpl @Inject constructor(
 
     override fun searchTvShows(query: String, page: Int): Observable<List<MovieDbTvShow>> {
         return service.searchTvShows(query, page).map { mapper.map(it) }
-    }
-
-    private fun mergeResults(newList: List<MovieDbTvShow>) {
-        newList.forEach { (list as ArrayList).add(it) }
     }
 }
--- app/src/main/java/com/taitascioredev/android/tvseriesdemo/feature/tvshowslist/ShowsListActionProcessor.kt
+++ app/src/main/java/com/taitascioredev/android/tvseriesdemo/feature/tvshowslist/ShowsListActionProcessor.kt
@@ -1,5 +1,6 @@
 package com.taitascioredev.android.tvseriesdemo.feature.tvshowslist
 
+import com.taitascioredev.android.tvseriesdemo.domain.model.MovieDbTvShow
 import com.taitascioredev.android.tvseriesdemo.feature.tvshowslist.usecase.GetPopularTvShowsUseCase
 import io.reactivex.ObservableTransformer
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -14,13 +15,16 @@ class ShowsListActionProcessor @Inject constructor(private val useCase: GetPopul
 
     private var currentPage = 1
 
+    private val list: List<MovieDbTvShow> = ArrayList()
+
     private fun loadShows(): ObservableTransformer<ShowsListAction, ShowsListResult> {
         return ObservableTransformer { action ->
             action.flatMap {
                 useCase.getPopularTvShows(currentPage)
                         .map {
                             currentPage++
-                            ShowsListResult.success(it)
+                            mergeResults(it)
+                            ShowsListResult.success(list)
                         }
                         .onErrorReturn { ShowsListResult.error(it) }
                         .subscribeOn(Schedulers.io())
@@ -34,5 +38,9 @@ class ShowsListActionProcessor @Inject constructor(private val useCase: GetPopul
         action.publish { shared ->
             shared.ofType(ShowsListAction::class.java).compose(loadShows())
         }
+    }
+
+    private fun mergeResults(newList: List<MovieDbTvShow>) {
+        newList.forEach { (list as java.util.ArrayList).add(it) }
     }
 }
